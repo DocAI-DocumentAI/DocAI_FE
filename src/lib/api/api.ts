208e95104e2c1b1666ead61b7ee698a6b2bf9647
Magic Flexing <134113309+Nguyen-Huy-Phuc@users.noreply.github.com>
2025-08-11 17:34:42 +0700

    Fix: ...

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://production.docai.asia/api",
   baseURL: "https://production.docai.asia/api",
+  // baseURL: "https://localhost:5001/api",
   headers: {
     "Content-Type": "application/json",
   },
